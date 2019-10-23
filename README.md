f9f9829b62c0cd798c953da17afb16319100d765
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2019-10-23 14:58:53 +0800

    Add oohttp tool

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # homebrew-tap
-Yuanzhen 's HomeBrew Tap
+Yuanzhen 's HomeBrew Tap repository
+
+## QuickStart
+
+### Tap This Repo
+
+```shell
+# (Setup this tap)
+brew tap zhouyuanzhen/tap
+
+brew update
+```
+
+### Play with homebrew
+
+```shell
+brew install oohttp
+```
+
+## End
+
+Enjoy with it:)
---
+++ oohttp.rb
@@ -0,0 +1,15 @@
+class Oowebserver < Formula
+    desc "oohttp is a golang binary tool for quickstart a local webserver"
+    homepage "https://github.com/zhouyuanzhen/oohttp"
+    url "https://github.com/zhouyuanzhen/oohttp/releases/download/v0.9.0/oohttp_macos"
+    version "v0.9.0"
+    sha256 "d9e9e1365491833a8cad4c98687af25407aec1aafa6dbd6c6d768ad9571d12a5"
+  
+    def install
+        bin.install "oohttp_macos"
+    end
+
+    test do
+        assert_match version.to_s, shell_output("#{bin}/oohttp_macos show usage info")
+    end
+end
