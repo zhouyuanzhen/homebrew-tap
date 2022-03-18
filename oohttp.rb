58717f2f40415f52c0156d9dd6ae8e6052d30e4f
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2022-03-18 21:00:02 +0800

    rename binary file to oo_http_macos

--- oohttp.rb
+++ oohttp.rb
@@ -1,15 +1,15 @@
 class Oohttp < Formula
     desc "oohttp is a golang binary tool for quickstart a local webserver"
     homepage "https://github.com/zhouyuanzhen/oohttp"
-    url "https://github.com/zhouyuanzhen/oohttp/releases/download/v0.9.0/oohttp_macos"
-    version "v0.9.0"
-    sha256 "d9e9e1365491833a8cad4c98687af25407aec1aafa6dbd6c6d768ad9571d12a5"
+    url "https://github.com/zhouyuanzhen/oohttp/releases/download/v0.9.1/oo_http_macos"
+    version "v0.9.1"
+    sha256 "5d556c0ca986b2487cfc63c35d0675579709a60e59c803a376aa2f19b27defce"
   
     def install
-        bin.install "oohttp_macos"
+        bin.install "oo_http_macos"
     end
 
     test do
-        assert_match version.to_s, shell_output("#{bin}/oohttp_macos show usage info")
+        assert_match version.to_s, shell_output("#{bin}/oo_http_macos show usage info")
     end
 end
